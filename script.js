9d5fcd6e272e39a68044fab411a6dee69110e1dd
jevenshirrell <jshirr790@west-mec.org>
2026-04-09 18:24:02 -0700

    Channel switching + typing anim
    Added functionality for typing animation and messages fading in one by one

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+let channels = {
+    "Home":['one', 'i'],
+    "About Me":[],
+    "Skills":[],
+    "Projects":[],
+    "Resume":[],
+    "Contact":[],
+}
+
+// profile popup
 $('#miniProf').on('click', function() {
     $('#profilePopup').fadeToggle(150);
+})
+// channels
+// messages
+$('.channel').on('click', function() {
+    $('#messagesWrapper').children().each(function() {
+        $(this).fadeOut(150, () => {
+            $(this).remove()
+        })
+    })
+    setTimeout(() => {
+        $('#animWrapper').fadeIn(150).css({'display':'flex'})
+        $.each(channels[this.textContent], (index, value) => {
+            setTimeout(() => {
+                $('#messagesWrapper').append(`<div class="msg card">
+                    <img src="./imgs/pfp.png" alt="Profile Picture">
+                    <div class="textWrapper">
+                        <h3 class="headerFont">Jeven Shirrell</h3>
+                        <p>${value}</p>
+                    </div>
+                </div>`)
+                $('#messagesWrapper').children().last().hide().delay(1000).fadeIn(500)
+            }, 1500 * index)
+        })
+        setTimeout(() => {$('#animWrapper').fadeOut(150)}, 1500 * channels[this.textContent].length)
+        
+    }, 150)
 })
